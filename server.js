234d62748625b59a9c20c2424b60502f57403ae2
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-04-22 10:25:27 -0600

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.get("/test", function (req, res) {
-  res.send("Hello World");
+app.get("/", function (req, res) {
+  res.send("Kevin Allred");
 });
 
 var server = app.listen(8081, function () {
